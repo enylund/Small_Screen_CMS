0f372c2ded751c4c531534f3cdc06acf05ffec99
enylund <eric.nylund@yale.edu>
2014-10-12 23:13:49 -0400

    bunch of changes

--- server.js
+++ server.js
@@ -16,80 +16,99 @@ var url_room = io.listen(server);
 // Tell Express to serve static assets
 router.use(express.static(path.resolve(__dirname, 'client')));
 
+
+// Connect to MongoDB
 MongoClient.connect('mongodb://mobilecomputing:asdf1234@ds043210.mongolab.com:43210/mobilescreens', function(err, db) {
       if (err) throw err;
       if (!err) {
         console.log("we are connected");
 
-      var sites = db.collection('listofurls');
-
-      url_room.on('connection', function(socket) {
-
-        var testTwo = sites.find().sort( { name:1 } );
+// create a shortcut to the collection
+var sites = db.collection('listofurls');
 
 
-          var test = sites.find().sort( { name: 1 } ).each(function(err, message) {
-            if (message != null) {
-                //console.log(message);
-                socket.emit('bdInfo', message);
-            }
-          });
+url_room.on('connection', function(socket) {
+	
+	socket.on('syncRequest', function(data){
+		socket.broadcast.emit('syncUpScreens');
+		console.log("message recieved");
+	});
 
-          socket.on('addtodb', function(data) {
-              
-              var newDoc = {
-                  siteurl: data.siteurl,
-                  timeval: data.timeval,
-                  active: 1,
-                  screen: data.screen
-              };
+      //Execute the below function to send student site urls to screen display pages
+      socket.on('connected', function(data) {
 
-              sites.insert(newDoc, function(err, inserted) {
-                if (err) throw err;
-              });
-          });
+          //set the data from the client that tells which screens info is needed
+          //query database for active urls
+          var screenThatsRequesting = data;
+          var queryScreen = sites.find(  { $and: [ { screen: screenThatsRequesting }, { active: 1 } ] }, {siteurl:1, timeval:1}).sort({positionVal: 1});
+		  var screenToEmitTo = "siteRotate"+screenThatsRequesting;
+            //iterate over db query to send to screens
+            queryScreen.toArray(function(err, documents) {
 
-          socket.on('updatedb', function(data) {
+                socket.emit(screenToEmitTo, documents);
 
-            var theSite = data.idToUpdate;
-            var theScreen = data.screen;
-            console.log(theSite);
+            });
+      });
 
-            sites.update({ $and: [ {siteurl: theSite},{screen: theScreen}]}, {$set: {active: 2}}, { multi: true }, function(err, updated) {
+      //initial database query for use on the index page of CMS
+      sites.find().sort( { positionVal: 1 } ).each(function(err, message) {
+          if (message != null) {
+
+              //Emit information to build the Index Page
+              if( message.siteurl != 'blank' ) {
+              socket.emit('bdInfo', message);
+              }
+          }
+      });
+
+      //receives index page database additions
+      socket.on('addtodb', function(data) {
+
+          //variable holds requested url position in scheduler queue
+          var positionMarker = data.positionVal;
+
+          sites.find({ active: 1 }).sort( { positionVal: 1 }).each(function(err, message) {
+              if (message != null) {
+                  if ( message.positionVal == positionMarker ) {
+                      console.log("duplicate reached");
+                      var positionMarkerNum = parseInt(positionMarker);
+                      console.log(positionMarkerNum);
+
+                      //This should update the whole queue. NOT WORKING
+                      sites.update({positionVal: {$gte: positionMarkerNum} }, { $inc: { positionVal: 1 }}, { multi: true}, function(err, updated) {
+                          if( err || !updated ) console.log("User not updated");
+                          else console.log("User updated");
+                      }); 
+                   }
+                }
+           });
+
+           //Build database addition json
+           var newDoc = {
+              siteurl: data.siteurl,
+              timeval: data.timeval,
+              active: 1,
+              screen: data.screen,
+              positionVal: data.positionVal
+           };
+
+           sites.insert(newDoc, function(err, inserted) {
+              if (err) throw err;
+            });
+       });
+
+       //Updates the DB when user removes a url from the queue
+       socket.on('updatedb', function(data) {
+
+          var theSite = data.idToUpdate;
+          var theScreen = data.screen;
+
+          sites.update({ $and: [ {siteurl: theSite},{screen: theScreen}]}, {$set: {active: 2}}, { multi: true }, function(err, updated) {
                 if( err || !updated ) console.log("User not updated");
                   else console.log("User updated");
-            });
-
           });
 
-          socket.on('updatedbTwo', function(data) {
-
-            var theSite = data.idToUpdate;
-            var theScreen = data.screen;
-            console.log(theSite);
-            console.log(theScreen);
-
-            sites.update({ $and: [ {siteurl: theSite},{screen: theScreen}]}, {$set: {active: 2}}, { multi: true }, function(err, updated) {
-                if( err || !updated ) console.log("User not updated");
-                  else console.log("User updated");
-            });
-
-          });
-
-          socket.on('updatedbThree', function(data) {
-
-            var theSite = data.idToUpdate;
-            var theScreen = data.screen;
-            console.log(theSite);
-            console.log(theScreen);
-
-            sites.update({ $and: [ {siteurl: theSite},{screen: theScreen}]}, {$set: {active: 2}}, { multi: true }, function(err, updated) {
-                if( err || !updated ) console.log("User not updated");
-                  else console.log("User updated");
-            });
-
-          });
-
+        });
 
       });
 
